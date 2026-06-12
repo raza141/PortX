76b684f55a18a2ab4041224569c62fb79ba68f43
Muhammad Ahmed Raza <106024393+raza141@users.noreply.github.com>
2026-06-12 10:12:19 +0400

    Add URL route for fee schedule rules API

--- operations/ibor/urls.py
+++ operations/ibor/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from operations.ibor.views import IborTradeCreateView
 from operations.ibor.views.fee_api_views import ibor_fee_quote_api
+from operations.ibor.views.fee_api_views import get_fee_schedule_rules_api
 
 app_name = "ibor"
 
@@ -9,5 +10,6 @@ urlpatterns = [
 
     path("trades/new/", IborTradeCreateView.as_view(), name="trade-create"),
     path("fees/quote/", ibor_fee_quote_api, name="fee-quote"),
+        path("fees/schedule-rules/", get_fee_schedule_rules_api, name="fee-schedule-rules"),
 
 ]
